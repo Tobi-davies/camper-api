2bcafe1c336702603f9b28b291198dedaaef941b
Adebayo <adebayotobiloba9@gmail.com>
2022-07-24 22:55:02 +0100

    server update

--- server.js
+++ server.js
@@ -2,12 +2,16 @@ const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
 // const logger = require("./middleware/logger");
-
-//Route files
-const bootcamps = require("./routes/bootcamps");
+const connectDB = require("./config/db");
 
 //LOAD ENV
 dotenv.config({ path: "./config/config.env" });
+
+//Connect to database
+connectDB();
+
+//Route files
+const bootcamps = require("./routes/bootcamps");
 
 const app = express();
 
@@ -22,7 +26,15 @@ if (process.env.NODE_ENV === "development") {
 app.use("/api/v1/bootcamps", bootcamps);
 
 const PORT = process.env.PORT || 5000;
-app.listen(
+
+const server = app.listen(
   PORT,
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
+
+//Handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  //close server and exit process
+  server.close(() => process.exit(1));
+});
